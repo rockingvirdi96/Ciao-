636d53bc4bb86e76cac6ad31bc28abf31f5cb53a
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2020-03-01 19:46:14 -0500

    new Design

--- functions/index.js
+++ functions/index.js
@@ -2,109 +2,26 @@ $ = e => {
   return document.querySelector(e);
 };
 
-var userInfo = {
+const userInfo = {
   name: "RUPINDER PAL SINGH",
   profession: "WEB DEVELOPER",
-  description: ".Ca va bien?."
-};
-var education = {
-  Course: ["Higher Secondary School", "Bachelors in Computer Science", "AEC Diploma I.T Programmer Analyst"],
-  Institute: ["C.T Public School", "D.A.V College", "La Salle College"],
-  Location: ["Jalandhar,India", "Jalandhar,India", "Montreal,Quebec"],
-  Duration: ["till 2014", "2014-2017", "Current"]
-};
-var projects = {};
-var skills = {};
+  address: "rupindervirdi96@gmail.com",
+  phoneNumber: "+1(438)998-1746",
 
-$(".main-content .introduction h1").innerText = userInfo.name;
-$(".main-content .introduction h3").innerText = userInfo.profession;
 
-$(".inn-left").onclick = function () {
-  var menuLeft = $(".menu-left-content");
-  var slideLeft = $(".slide-left");
-  if (!menuLeft.classList.contains("menu-content-animation")) {
-    menuLeft.classList.add("menu-content-animation");
-    slideLeft.classList.add("slide-opacity-action");
-  }
 };
 
-$(".inn-right").onclick = function () {
-  var menuRight = $(".menu-right-content");
-  var slideRight = $(".slide-right");
-  if (!menuRight.classList.contains("menu-content-animation")) {
-    menuRight.classList.add("menu-content-animation");
-    slideRight.classList.add("slide-opacity-action");
-  }
+const education = {
+  Course: ["Bachelors in Computer Science", "Information Technology Programmer Analyst"],
+  Institute: ["D.A.V College", "LaSalle College"],
+  Location: ["Jalandhar,India", "Montreal,Quebec"],
+  Duration: ["04/2014-04/2017", "01/2019 - 04/2020"]
 };
 
-$(".menu-left-content .close").onclick = function () {
-  var menuLeft = $(".menu-left-content");
-  var slideLeft = $(".slide-left");
-  if (menuLeft.classList.contains("menu-content-animation")) {
-    setTimeout(function () {
-      menuLeft.classList.remove("menu-content-animation");
-    }, 300);
-    setTimeout(function () {
-      slideLeft.classList.remove("slide-opacity-action");
-    }, 200);
-    slideLeft.classList.remove("slide-left-action");
-  }
-};
-
-$(".menu-right-content .close").onclick = function () {
-  var menuRight = $(".menu-right-content");
-  var slideRight = $(".slide-right");
-  if (menuRight.classList.contains("menu-content-animation")) {
-    if (slideRight.classList.contains("slide-right-action")) {
-      setTimeout(function () {
-        menuRight.classList.remove("menu-content-animation");
-      }, 300);
-      setTimeout(function () {
-        slideRight.classList.remove("slide-right-action");
-        slideRight.classList.remove("slide-opacity-action");
-      }, 200);
-    } else {
-      setTimeout(function () {
-        menuRight.classList.remove("menu-content-animation");
-      }, 100);
-      slideRight.classList.remove("slide-opacity-action");
-    }
-  }
-};
-
-listt = $(".menu-right-content ul").children;
-for (i = 0; i < listt.length; i++) {
-  listt[i].onclick = function () {
-    var slideRight = $(".slide-right");
-    if (!slideRight.classList.contains("slide-right-action")) {
-      slideRight.classList.add("slide-right-action");
-    } else {
-      slideRight.classList.remove("slide-right-action");
-      setTimeout(function () {
-        slideRight.classList.toggle("slide-right-action");
-      }, 200);
-    }
-  };
-}
-
-for (var x = 0; x < education.Course.length; x++) {
-  $(".slides .slide-left .left-slide-content").innerHTML += `<ul type="none" class="course-module"><li>${education.Course[x]}</li><li>${education.Institute[x]}</li><li>${education.Location[x]}</li><li>${education.Duration[x]}</li></ul>`;
-};
-list = $(".menu-left-content ul").children;
-for (i = 0; i < list.length; i++) {
-  list[i].onclick = function () {
-    x = true;
-    var slideLeft = $(".slide-left");
-    if (!slideLeft.classList.contains("slide-left-action")) {
-      slideLeft.classList.add("slide-left-action");
-    }
-    else {
-      slideLeft.classList.remove("slide-left-action");
-      $(".slides .slide-left .left-slide-content").style.display = "none";
-      setTimeout(function () {
-        slideLeft.classList.toggle("slide-left-action");
-        $(".slides .slide-left .left-slide-content").style.display = "";
-      }, 300);
-    }
-  };
+const experience = {
+  status: [],
+  Company: [],
+  Location: ["Jalandhar,India", "Montreal,Quebec"],
+  Duration: ["04/2014-04/2017", "01/2019 - 04/2020"],
+  Description: []
 }
